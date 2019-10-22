e5184ea86f04c765dc8423be117ba7110058ddac
Ieida <37672966+Ieida@users.noreply.github.com>
2019-10-22 12:21:29 +0200

    Player Controller

--- Scripts/Player/PlayerController.cs
+++ Scripts/Player/PlayerController.cs
@@ -3,19 +3,31 @@ using System;
 
 public class PlayerController : KinematicBody2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    [Export] float gravity;
+    [Export] float mass;
+    [Export] float movementSpeed;
+    [Export] float dashForce;
+    [Export] float kbInterp;
+    [Export] float gravityInterp;
+    float x;
+    float y;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+    public override void _PhysicsProcess(float delta)
     {
-        
+        Vector2 jumpDir = new Vector2(Input.GetActionStrength("left_c")-Input.GetActionStrength("right_c"), -Input.GetActionStrength("up_c")).Normalized();
+        Vector2 dir = new Vector2(Input.GetActionStrength("right")-Input.GetActionStrength("left"), Input.GetActionStrength("up"));
+        if(IsOnFloor() && jumpDir != Vector2.Zero)
+        {
+            x = Mathf.Lerp(x, dir.y*((jumpDir.x*dashForce)*(mass*gravity)), delta*gravityInterp);
+            y = Mathf.Lerp(y, dir.y*((jumpDir.y*dashForce)*(mass*gravity)), delta*gravityInterp);
+            MoveAndSlide(new Vector2(x, -y), new Vector2(0.0f, -1.0f));
+            GD.Print(jumpDir.y);
+        }
+        else
+        {
+            x = Mathf.Lerp(x, (dir.x*movementSpeed)/mass, delta*kbInterp);
+            y = Mathf.Lerp(y, mass*gravity, delta*gravityInterp);
+            MoveAndSlide(new Vector2(x, -y), new Vector2(0.0f, -1.0f));
+        }
     }
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
 }
